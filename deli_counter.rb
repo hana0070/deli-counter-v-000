04236a7f4cb8119d52cdf6fa11775386a82b7cb5
HANA LEANDER <hana0070@gmail.com>
2019-04-11 01:43:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(deli)
   else
     current_line = "The line is currently:"
     deli.each.with_index(1) do |name, i|
-      current_line << "#{i}. #{name}"
+      current_line << " #{i}. #{name}"
     end
     puts current_line
   end
