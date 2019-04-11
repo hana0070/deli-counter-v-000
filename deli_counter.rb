ddac79aab39112f78c5e67944202b6ae7c87a72e
HANA LEANDER <hana0070@gmail.com>
2019-04-11 01:53:53 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -22,6 +22,6 @@ def now_serving(deli)
     puts "There is nobody waiting to be served!"
   else
     deli.shift
-    puts "Currently serving #{deli[0]}."
+    puts "Currently serving #{deli[1]}."
   end
 end
