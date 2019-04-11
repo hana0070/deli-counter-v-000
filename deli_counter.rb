16fce4de3540ab9d1b1c733d6df4542dacf15e4b
HANA LEANDER <hana0070@gmail.com>
2019-04-11 01:44:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,8 +11,8 @@ def line(deli)
   end
 end
 
-def take_a_number(katz_deli, name)
-  if katz_deli = []
+def take_a_number(deli, name)
+  if deli.empty?
     puts "there is nobody in line"
   else
     puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
