27ed8e3c965af8a0b0ef3de3ae8132d31b000243
HANA LEANDER <hana0070@gmail.com>
2019-04-11 01:49:45 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,6 +19,11 @@ def take_a_number(deli, name)
   end
 end
 
+#def take_a_number(deli, name)
+  #deli << name
+  #puts "Welcome, #{name}. You are number #{deli.length} in line."
+#end
+
 def now_serving(katz_deli)
   if katz_deli = []
     puts "There is nobody waiting to be served!"
